41dd7ad9a274cade2b5e41afde5dfbd1f7cf9585
DreamPaste <bb7788@naver.com>
2025-07-15 20:42:42 +0000

    fix: Removed Storybook-related files from ESLint's ignorePatterns.

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -5,11 +5,12 @@ module.exports = {
   parserOptions: {
     project: './tsconfig.json',
   },
-  ignorePatterns: [
-    'stylelint.config.js',
-    'postcss.config.mjs',
+  ignorePatterns: ['stylelint.config.js', 'postcss.config.mjs'],
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
   ],
-  extends: ['next', 'next/core-web-vitals', 'plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended', 'plugin:storybook/recommended'],
   overrides: [
     {
       // 이 패턴에 걸리는 파일은 타입 검사(parserOptions.project) 하지 않음
